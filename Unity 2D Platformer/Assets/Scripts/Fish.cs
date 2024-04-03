9f86ef7b42e51e28653cfb8a0e799a895365590a
Kimsumi111 <rlatnal234@naver.com>
2024-04-03 23:37:37 +0900

    fishmove...ing
    Dancing!

--- Unity 2D Platformer/Assets/Scripts/Fish.cs
+++ Unity 2D Platformer/Assets/Scripts/Fish.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    public float moveSpeed = 1f;
+    public float moveSpeed;
 
     void Update()
     {
-        StartCoroutine(MoveFish());
-    }
-    private IEnumerator MoveFish(){
-
         transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-
-        yield return new WaitForSeconds(3f);
-
+        Invoke("TurnFish", 3f);
+    }
+    void TurnFish(){
         moveSpeed *= -1f;
         Vector3 scale = transform.localScale;
         scale.x *= -1f;
         transform.localScale = scale;
-        
-        yield return new WaitForSeconds(3f);
     }  
 }
